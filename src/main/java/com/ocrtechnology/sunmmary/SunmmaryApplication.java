fd06944cd6625313282a67a404bfafd8a3ac8900
panle <m15570719300@163.com>
2019-04-02 09:34:33 +0800

    Initial commit

---
+++ src/main/java/com/ocrtechnology/sunmmary/SunmmaryApplication.java
@@ -0,0 +1,13 @@
+package com.ocrtechnology.sunmmary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunmmaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunmmaryApplication.class, args);
+    }
+
+}
